99ce7f5d9435163356b70b9b47f960042977e4b9
Rachit Dhaka <rachitdhaka29@gmail.com>
2025-04-14 19:34:52 +0530

    Portfolio

---
+++ src/components/Social.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className='text-[16px] font-medium text-[#9b9b9b] flex justify-center gap-7'>
+        <a href="">Twitter</a>
+        <a href="">LinkedIn</a>
+        <a href="">Github</a>
+        <a href="">Coding</a>
+    </div>
+  )
+}
+
+export default Social
